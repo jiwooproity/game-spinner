312b00ef8817a143b6b49b17c103d6a2a3503600
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2025-05-08 00:15:24 +0900

    Feat(audio.js): 소리 활성화 / 비활성화 기능 추가 - 돌림판 하단에 소리를 활성화 혹은 비활성화 가능한 버튼 추가

--- javascript/audio.js
+++ javascript/audio.js
@@ -1,8 +1,26 @@
+const speaker = document.querySelector(".speaker");
+
+let speakerStatus = false; // default is on
+
 class SoundEffect extends Audio {
   constructor(fileName, volum = 0.5) {
     super();
 
     this.src = fileName;
-    this.volume = volum;
+    this.volume = speakerStatus ? volum : 0;
   }
 }
+
+const handleSpeaker = () => {
+  speaker.src = `./images/speaker-${speakerStatus ? "off" : "on"}.svg`;
+  storage.setItem("speaker-status", "true");
+  speakerStatus = !speakerStatus;
+};
+
+const initSpeaker = () => {
+  speakerStatus = storage.getItem("speaker-status", "boolean") ?? false;
+  speaker.src = `./images/speaker-${speakerStatus ? "on" : "off"}.svg`;
+  speaker.addEventListener("click", handleSpeaker);
+};
+
+document.addEventListener("DOMContentLoaded", initSpeaker);
